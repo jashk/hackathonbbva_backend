8cf8c3e96bfe0323153c5b2cd12a298432b77dff
Fidel Aquino <fidel.aquino.dev@gmail.com>
2018-09-09 00:46:48 -0500

    Add Layout validations

--- app/Http/Controllers/QRController.php
+++ app/Http/Controllers/QRController.php
@@ -4,14 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Endroid\QrCode\QrCode;
-use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
 
 class QRController extends Controller
 {
     const OPERATION_TYPE = '0001';
-    // const DEBIT_CARD = 'TD';
-    // const CREDIT_CARD = 'TC';
-    // const CLABE = 'CL';
     const BANK_BBVA = '00012';
     const COUNTRY = 'MX';
     const CURRENCY = 'MXN';
@@ -22,35 +19,55 @@ class QRController extends Controller
     public function generate(Request $request) {
 
         $trans = $request->all();
-        // $request->validate([
-        //     'alias' => 'string|max:100',
-        //     'account' => 'required|string|max:20',
-        //     'account_type' => 'required|string',
-        //     'reference' => 'string',
-        //     'account_holder_name' => 'required|string|max:20',
-        //     'amount' => 'string|max:100',
-        // ]);
+
+        $validator = Validator::make($request->all(), [
+            'alias' => 'string',
+            'account' => 'required|string',
+            'account_type' => 'required|string',
+            'reference' => 'string',
+            'account_holder_name' => 'required|string',
+            'amount' => 'string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                    "status" => false,
+                    "error" => $validator->messages()->first()
+                ], 400);
+
+        }
+        if(!empty($trans['alias']) && (strlen($trans['alias']) < 3  || strlen($trans['alias']) > 100 || preg_match('/(^([a-zA-Z ]+)?$)/u', $trans['alias']) == false)){
+            return response()->json([
+                "status" => false,
+                "error" => "La nombre del beneficiario debe contener en 3 y 20 caracteres. No debe contener caracteres acentuados, especiales o ñ"
+            ], 400);
+        }
 
 
+        if((strlen($trans['account']) < 10  || strlen($trans['account']) > 20 || preg_match('/^[a-zA-Z0-9]*$/', $trans['account']) == false)){
+            return response()->json([
+                "status" => false,
+                "error" => "El número de cuenta consta de 18 dígitos para cuenta CLABE y 16 dígitos para tarjeta de crédito o débito"
+            ], 400);
+        }
 
         if(!in_array($trans['account_type'], array_keys(self::$ACCOUNT_TYPES))){
-            return response()->json(["status" => false, "error" => "El tipo de cuenta no es soportado"], 400);
+            return response()->json(["status" => false, "error" => "El tipo de cuenta no es soportado, tutilice DEBIT_CARD,  o CLABE"], 400);
+        }
+
+        if(!empty($trans['reference']) && (strlen($trans['reference']) < 6  || strlen($trans['reference']) > 7 || preg_match('/^[a-zA-Z0-9]*$/', $trans['reference']) == false)){
+            return response()->json(["status" => false, "error" => "La referencia debe contener en 6 y 7 caracteres"], 400);
         }
 
         if(!empty($trans['amount'])){
             $trans['amount'] = number_format((float)$trans['amount'], 2, '.', '');
-
-            if($trans['amount'] <= 0 ){
-                return response()->json(["status" => false, "error" => "El monto a pagar no puede ser igual o menos a cero"], 400);
+            if($trans['amount'] <= 0 || strlen($trans['amount']) > 100){
+                return response()->json(["status" => false, "error" => "El monto a pagar no puede ser igual o menor a cero y no debe ser mayor de 100 caracteres"], 400);
             }
         }
 
-        if(!empty($trans['reference']) && (strlen($trans['reference']) < 6  || strlen($trans['reference']) > 7)){
-            return response()->json(["status" => false, "error" => "La referencia debe contener en 6 y 7 carácteres"], 400);
-        }
-
-        if(!empty($trans['account_holder_name']) && (strlen($trans['account_holder_name']) < 3  || strlen($trans['account_holder_name']) > 20)){
-            return response()->json(["status" => false, "error" => "La nombre del beneficiario debe contener en 3 y 20 carácteres"], 400);
+        if(!empty($trans['account_holder_name']) && (strlen($trans['account_holder_name']) < 3  || strlen($trans['account_holder_name']) > 20 || preg_match('/(^([a-zA-Z ]+)?$)/u', $trans['account_holder_name']) == false)){
+            return response()->json(["status" => false, "error" => "El nombre del beneficiario debe contener en 3 y 20 caracteres.Sin apellidos. No debe contener caracteres acentuados, especiales o ñ"], 400);
         }
 
         $layout = [
@@ -69,7 +86,6 @@ class QRController extends Controller
         ];
 
         $tras_string = json_encode($layout);
-
         $qrCode = new QrCode($tras_string);
 
         return response($qrCode->writeString())
